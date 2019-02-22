db95eaab8cc0ea05cbcc97d28c8e565ce69db717
nhattien <nhattien2307@gmail.com>
2019-02-22 13:40:10 +0700

    finish chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,14 @@
 class UsersController < ApplicationController
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t "message_err"
-    redirect_to root_path
+  before_action :load_user, except: %i(index new create)
+  before_action :logged_in_user, except: %i(new create show)
+  before_action :correct_user, only: %i(edit update)
+  before_action :admin_user, only: :destroy
+
+  def index
+    @users = User.paginate page: params[:page], per_page: Settings.user.per_page
   end
+
+  def show; end
 
   def new
     @user = User.new
@@ -21,9 +25,53 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t "profile_updated"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:success] = t "delete_user"
+      redirect_to users_path
+    else
+      flash[:danger] = t "unsuccessfully_user"
+      redirect_to root_path
+    end
+  end
+
   private
+
+  def load_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t "no_data"
+    redirect_to root_path
+  end
+
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "login_plz"
+    redirect_to login_path
+  end
+
+  def correct_user
+    redirect_to root_path unless current_user?(@user)
+  end
+
+  def admin_user
+    redirect_to root_path unless current_user.admin?
   end
 end
